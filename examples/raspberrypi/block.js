1dfa919f159511e724814016bf084fd23b7c644b
Cavon <cavonxx@gmail.com>
2019-01-17 12:13:33 +0800

    update

---
+++ examples/raspberrypi/block.js
@@ -0,0 +1,23 @@
+RaspberryPi.examplesHasKit.block = {}
+
+RaspberryPi.examplesHasKit.block['Blink LED'] = {
+    "name": 'Blink LEDxxxxxxx',
+    "project": {
+        code: {
+            code: "<xml xmlns='http://www.w3.org/1999/xhtml'><block type='basic_start' id='qrswBI$2J**dCagSbF$0' x='-230' y='-290'></block><block type='basic_forever' id='(.X9-M;L7,PGS[je0yNt' x='-190' y='-170'><statement name='statement'><block type='controls_repeat_ext' id='pI6q7blNr1(Qw#)+:Ch}'><value name='TIMES'><shadow type='math_number' id='BI!m:|9C0W6`HB=kOHVE'><field name='NUM'>10</field></shadow></value><statement name='DO'><block type='basic_print' id='-H4?7gf::[VXZ2!H2.iX'><value name='msg'><shadow type='text' id='5K=XYU9LPM-{r`WsT:r^'><field name='TEXT'>abc</field></shadow></value></block></statement><next><block type='basic_delay' id=';DSq;%l?o;y)|v9MuRGq'><value name='ms'><shadow type='math_number' id='VMgAzxp|_N}ERGy}}kPR'><field name='NUM'>100</field></shadow></value></block></next></block></statement></block></xml>",
+            libs: {}
+        },
+        datetime: 1542880086479,
+        device: "raspberrypi",
+        iot: [],
+        name: "c",
+        platform: "blockcode",
+        readOnly: false,
+        remote: {},
+        route: { device: { x: 453, y: 351, id: 453 }, currentModules: {} }
+    }
+}
+
+RaspberryPi.examplesHasKit.block['Breath LED'] = {
+    "name": 'Breath LEDxxxxxxxxx'
+}
