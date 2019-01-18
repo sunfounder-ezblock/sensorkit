d38453de758f53c5812572e87e8599d5064a44ce
Cavon <cavonxx@gmail.com>
2019-01-18 10:50:24 +0800

    fix msgfiles

--- msg/en.js
+++ msg/en.js
@@ -1,80 +1,72 @@
-'use strict';
+Local.en.MSG.catSensorkit = "Sensor Kit";
 
-goog.provide('Blockly.Msg.actuators.en');
+Local.en.SENSORKIT_LED_MODULE_SET_VALUE_TITLE1 = "LED module set ";
+Local.en.SENSORKIT_LED_MODULE_SET_VALUE_TITLE2 = "status";
+Local.en.SENSORKIT_LED_MODULE_SET_VALUE_TOOLTIP = "Set LED to ON or OFF.(0 is OFF, 1 is ON)";
 
-goog.require('Blockly.Msg');
+Local.en.SENSORKIT_RGB_SET_VALUE_TITLE1 = "RGB module R";
+Local.en.SENSORKIT_RGB_SET_VALUE_TITLE2 = "G";
+Local.en.SENSORKIT_RGB_SET_VALUE_TITLE3 = "B";
+Local.en.SENSORKIT_RGB_SET_VALUE_TITLE4 = "set colour to";
+Local.en.SENSORKIT_RGB_SET_VALUE_TOOLTIP = "Set RGB color";
 
-MSG.catSensorkit = "Sensor Kit";
+Local.en.SENSORKIT_BUTTON_GET_VALUE_TITLE1 = "Button get";
+Local.en.SENSORKIT_BUTTON_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_BUTTON_GET_VALUE_TOOLTIP = "Get button value.0(button pressed) or 1(button released)";
 
+Local.en.SENSORKIT_TILTSWITCH_GET_VALUE_TITLE1 = "Tilt switch get";
+Local.en.SENSORKIT_TILTSWITCH_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_TILTSWITCH_GET_VALUE_TOOLTIP = "Get tilt switch value.";
 
+Local.en.SENSORKIT_VIBRATIONSWITCH_GET_VALUE_TITLE1 = "Vibrationt switch";
+Local.en.SENSORKIT_VIBRATIONSWITCH_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_VIBRATIONSWITCH_GET_VALUE_TOOLTIP = "Get vibration switch value.";
 
-Blockly.Msg.SENSORKIT_LED_MODULE_SET_VALUE_TITLE1 = "LED module set ";
-Blockly.Msg.SENSORKIT_LED_MODULE_SET_VALUE_TITLE2 = "status";
-Blockly.Msg.SENSORKIT_LED_MODULE_SET_VALUE_TOOLTIP = "Set LED to ON or OFF.(0 is OFF, 1 is ON)";
+Local.en.SENSORKIT_BUZZER_SET_STATUS_TITLE1 = "Buzzer";
+Local.en.SENSORKIT_BUZZER_SET_STATUS_TITLE2 = "play";
+Local.en.SENSORKIT_BUZZER_SET_STATUS_TITLE3 = "for";
+Local.en.SENSORKIT_BUZZER_SET_STATUS_TOOLTIP = "Buzzer play note for a beat";
 
-Blockly.Msg.SENSORKIT_RGB_SET_VALUE_TITLE1 = "RGB module R";
-Blockly.Msg.SENSORKIT_RGB_SET_VALUE_TITLE2 = "G";
-Blockly.Msg.SENSORKIT_RGB_SET_VALUE_TITLE3 = "B";
-Blockly.Msg.SENSORKIT_RGB_SET_VALUE_TITLE4 = "set colour to";
-Blockly.Msg.SENSORKIT_RGB_SET_VALUE_TOOLTIP = "Set RGB color";
+Local.en.SENSORKIT_JOYSTICK_GET_VALUE_TITLE1 = "Joystick get";
+Local.en.SENSORKIT_JOYSTICK_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_JOYSTICK_GET_VALUE_TOOLTIP = "Get joystick value";
 
-Blockly.Msg.SENSORKIT_BUTTON_GET_VALUE_TITLE1 = "Button get";
-Blockly.Msg.SENSORKIT_BUTTON_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_BUTTON_GET_VALUE_TOOLTIP = "Get button value.0(button pressed) or 1(button released)";
+Local.en.SENSORKIT_JOYSTICK_GET_STATUS_TITLE1 = "Joystick get status  X";
+Local.en.SENSORKIT_JOYSTICK_GET_STATUS_TITLE2 = "Y";
+Local.en.SENSORKIT_JOYSTICK_GET_STATUS_TITLE3 = "BT";
+Local.en.SENSORKIT_JOYSTICK_GET_STATUS_TOOLTIP = "Get joystick status";
 
-Blockly.Msg.SENSORKIT_TILTSWITCH_GET_VALUE_TITLE1 = "Tilt switch get";
-Blockly.Msg.SENSORKIT_TILTSWITCH_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_TILTSWITCH_GET_VALUE_TOOLTIP = "Get tilt switch value.";
+Local.en.SENSORKIT_POTENTIOMETER_GET_VALUE_TITLE1 = "Potentiometer get";
+Local.en.SENSORKIT_POTENTIOMETER_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_POTENTIOMETER_GET_VALUE_TOOLTIP = "Get potentiometer analog value.";
 
-Blockly.Msg.SENSORKIT_VIBRATIONSWITCH_GET_VALUE_TITLE1 = "Vibrationt switch";
-Blockly.Msg.SENSORKIT_VIBRATIONSWITCH_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_VIBRATIONSWITCH_GET_VALUE_TOOLTIP = "Get vibration switch value.";
+Local.en.SENSORKIT_SOUNDSENSOR_GET_VALUE_TITLE1 = "Sound sensor get";
+Local.en.SENSORKIT_SOUNDSENSOR_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_SOUNDSENSOR_GET_VALUE_TOOLTIP = "Get sound analog value.";
 
-Blockly.Msg.SENSORKIT_BUZZER_SET_STATUS_TITLE1 = "Buzzer";
-Blockly.Msg.SENSORKIT_BUZZER_SET_STATUS_TITLE2 = "play";
-Blockly.Msg.SENSORKIT_BUZZER_SET_STATUS_TITLE3 = "for";
-Blockly.Msg.SENSORKIT_BUZZER_SET_STATUS_TOOLTIP = "Buzzer play note for a beat";
+Local.en.SENSORKIT_PHOTORESISTOR_GET_VALUE_TITLE1 = "Photoresistor get";
+Local.en.SENSORKIT_PHOTORESISTOR_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_PHOTORESISTOR_GET_VALUE_TOOLTIP = "Get photoresisotr analog value.";
 
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_VALUE_TITLE1 = "Joystick get";
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_VALUE_TOOLTIP = "Get joystick value";
+Local.en.SENSORKIT_TOUCHSWITCH_GET_VALUE_TITLE1 = "Touch switch get";
+Local.en.SENSORKIT_TOUCHSWITCH_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_TOUCHSWITCH_GET_VALUE_TOOLTIP = "Get touch switch value.";
 
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_STATUS_TITLE1 = "Joystick get status  X";
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_STATUS_TITLE2 = "Y";
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_STATUS_TITLE3 = "BT";
-Blockly.Msg.SENSORKIT_JOYSTICK_GET_STATUS_TOOLTIP = "Get joystick status";
+Local.en.SENSORKIT_ULTRASONIC_GET_VALUE_TITLE1 = "ultrasonic ranging get distance";
+Local.en.SENSORKIT_ULTRASONIC_GET_VALUE_TITLE2 = "trig";
+Local.en.SENSORKIT_ULTRASONIC_GET_VALUE_TITLE3 = "echo";
+Local.en.SENSORKIT_ULTRASONIC_VALUE_TOOLTIP = " Get ultrasonic ranging value.";
 
-Blockly.Msg.SENSORKIT_POTENTIOMETER_GET_VALUE_TITLE1 = "Potentiometer get";
-Blockly.Msg.SENSORKIT_POTENTIOMETER_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_POTENTIOMETER_GET_VALUE_TOOLTIP = "Get potentiometer analog value.";
+Local.en.SENSORKIT_DS18B20_GET_VALUE_TITLE1 = "DS18B20 Get temperature(to GPIO 4)";
+Local.en.SENSORKIT_DS18B20_GET_VALUE_TOOLTIP = "Get DS18B20 value (Connect GPIO 4)";
 
-Blockly.Msg.SENSORKIT_SOUNDSENSOR_GET_VALUE_TITLE1 = "Sound sensor get";
-Blockly.Msg.SENSORKIT_SOUNDSENSOR_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_SOUNDSENSOR_GET_VALUE_TOOLTIP = "Get sound analog value.";
+Local.en.SENSORKIT_SERVO_SET_ANGLE_TITLE1 = "Servo set ";
+Local.en.SENSORKIT_SERVO_SET_ANGLE_TITLE2 = "angle";
+Local.en.SENSORKIT_SERVO_SET_ANGLE_TOOLTIP = "Set servo angle";
 
-Blockly.Msg.SENSORKIT_PHOTORESISTOR_GET_VALUE_TITLE1 = "Photoresistor get";
-Blockly.Msg.SENSORKIT_PHOTORESISTOR_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_PHOTORESISTOR_GET_VALUE_TOOLTIP = "Get photoresisotr analog value.";
+Local.en.SENSORKIT_MOISTURE_SENSOR_GET_VALUE_TITLE1 = "Moisture sensor get";
+Local.en.SENSORKIT_MOISTURE_SENSOR_GET_VALUE_TITLE2 = "value";
+Local.en.SENSORKIT_MOISTURE_SENSOR_GET_VALUE_TOOLTIP = "Get moisture sensor value";
 
-Blockly.Msg.SENSORKIT_TOUCHSWITCH_GET_VALUE_TITLE1 = "Touch switch get";
-Blockly.Msg.SENSORKIT_TOUCHSWITCH_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_TOUCHSWITCH_GET_VALUE_TOOLTIP = "Get touch switch value.";
-
-Blockly.Msg.SENSORKIT_ULTRASONIC_GET_VALUE_TITLE1 = "ultrasonic ranging get distance";
-Blockly.Msg.SENSORKIT_ULTRASONIC_GET_VALUE_TITLE2 = "trig";
-Blockly.Msg.SENSORKIT_ULTRASONIC_GET_VALUE_TITLE3 = "echo";
-Blockly.Msg.SENSORKIT_ULTRASONIC_VALUE_TOOLTIP = " Get ultrasonic ranging value.";
-
-Blockly.Msg.SENSORKIT_DS18B20_GET_VALUE_TITLE1 = "DS18B20 Get temperature(to GPIO 4)";
-Blockly.Msg.SENSORKIT_DS18B20_GET_VALUE_TOOLTIP = "Get DS18B20 value (Connect GPIO 4)";
-
-Blockly.Msg.SENSORKIT_SERVO_SET_ANGLE_TITLE1 = "Servo set ";
-Blockly.Msg.SENSORKIT_SERVO_SET_ANGLE_TITLE2 = "angle";
-Blockly.Msg.SENSORKIT_SERVO_SET_ANGLE_TOOLTIP = "Set servo angle";
-
-Blockly.Msg.SENSORKIT_MOISTURE_SENSOR_GET_VALUE_TITLE1 = "Moisture sensor get";
-Blockly.Msg.SENSORKIT_MOISTURE_SENSOR_GET_VALUE_TITLE2 = "value";
-Blockly.Msg.SENSORKIT_MOISTURE_SENSOR_GET_VALUE_TOOLTIP = "Get moisture sensor value";
-
-Blockly.Msg.SENSORKIT_ADXL345_GET_VALUE_TITLE1 = "ADXL345 get value";
-Blockly.Msg.SENSORKIT_ADXL345_GET_VALUE_TOOLTIP = "Get ADXL345 value";
+Local.en.SENSORKIT_ADXL345_GET_VALUE_TITLE1 = "ADXL345 get value";
+Local.en.SENSORKIT_ADXL345_GET_VALUE_TOOLTIP = "Get ADXL345 value";
